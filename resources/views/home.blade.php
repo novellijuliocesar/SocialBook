5774400971362b3c618d8bc35280a404225e23d5
Julio Cesar Novelli <novellijuliocesar@hotmail.com>
2020-09-30 12:05:56 +0200

    A falta de diseñar un buscador de publicaciones

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,23 +4,6 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-        
-            <!-- Buscador de usuarios -->
-            <div class="card-body">
-                <div class="finder">
-                    <form action="{{route('user.showUsers')}}" method="GET" id="search-users">   
-                        @csrf
-                        <div class="form-group row"> 
-                            <div class="col-md-6">
-                                <input type="text" id="search" placeholder="Buscar..." class="form-control"/>
-                            </div>    
-                            <div class="form-group col btn-search">
-                                <input type="submit" value="Buscar" class="btn btn-success">
-                            </div> 
-                        </div>    
-                    </form>
-                </div>
-            </div>
 
             <!--Muestra la Publicación -->
             @foreach ($posts as $post)
